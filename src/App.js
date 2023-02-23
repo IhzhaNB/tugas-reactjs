2d52c778bc160d1a4120676ac89ef02ffa5dbb3b
IhzhaNB <ihzhanauvalb@gmail.com>
2023-02-23 12:26:01 +0700

    fourth Tas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Newsapi from "./components/thirdtask/Newsapi";
+import Apinews from "./components/fourthtask/Apinews";
+// import Newsapi from "./components/thirdtask/Newsapi";
 
 function App() {
   return (
     <>
       <div className="App">
-        <Newsapi />
+        {/* <Newsapi /> */}
+        <Apinews />
       </div>
     </>
   );
